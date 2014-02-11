199ad444fac9621f8a15cec276d7c23f8078bc01
Wim Leers <wim.leers@acquia.com>
2014-02-11 16:17:43 -0500

    Upgrade NewCacheApiSniff to use the new capabilities of FunctionReplacementSniff.

--- Drupal7to8/Sniffs/Cache/NewCacheApiSniff.php
+++ Drupal7to8/Sniffs/Cache/NewCacheApiSniff.php
@@ -18,13 +18,17 @@
  */
 class Drupal7to8_Sniffs_Cache_NewCacheApiSniff extends Drupal7to8_Sniffs_Functions_FunctionReplacementSniff {
 
-  protected $message = 'Upgrade cache_(get|set()) calls: https://drupal.org/node/1766152';
+  protected $message = '!function() has been removed in the new Cache API: https://drupal.org/node/1766152';
 
-  protected $code = 'NewCacheApi';
+  protected $code = 'NewCacheAPI';
 
   protected $forbiddenFunctions = array(
     'cache_get' => '\Drupal::cache()->get',
     'cache_set' => '\Drupal::cache()->set',
+    // Ideally we'd automatically fix cache_clear_all() calls too, but in some
+    // cases cache tags should be used, and it's impossible to automatically
+    // determine which cache tags should be used.
+    'cache_clear_all' => NULL,
   );
 
   protected $dynamicArgumentReplacements = array(

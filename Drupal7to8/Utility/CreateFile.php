4ae3870a046b321a23a5dc7a183d3ced710a9ce4
xjm <xjm@65776.no-reply.drupal.org>
2014-02-12 15:55:34 -0500

    Add a static method to convert underscore_names to CamelCase.

--- Drupal7to8/Utility/CreateFile.php
+++ Drupal7to8/Utility/CreateFile.php
@@ -28,11 +28,32 @@ class Drupal7to8_Utility_CreateFile {
    * @return string|null
    *   The PHP code with the tokens replaced, or NULL on failure.
    */
-  static public function replaceTokens($boilerPath, array $boilerTokens) {
+  public static function replaceTokens($boilerPath, array $boilerTokens) {
     if ($boilerplate = file_get_contents($boilerPath)) {
       return str_replace(array_keys($boilerTokens), array_values($boilerTokens), $boilerplate);
     }
   }
 
+  /**
+   * Converts an underscore-separated name to CamelCase.
+   *
+   * @param string $name
+   *   An underscore-separated string (machine name, variable name, etc.).
+   * @param bool $lowerCamel
+   *   Boolean flag to convert to lowerCamel instead of UpperCamel. Defaults to
+   *   FALSE.
+   *
+   * @return
+   *   The converted string.
+   */
+  public static function camelUnderscores($name, $lowerCamel = FALSE) {
+    $pieces = explode('_', $name);
+    array_walk($pieces, 'strtolower');
+    array_walk($pieces, 'ucfirst');
+    if ($lowerCamel) {
+      $pieces[0] = strtolower($pieces[0]);
+    }
+    return implode('', $pieces);
+  }
 
 }

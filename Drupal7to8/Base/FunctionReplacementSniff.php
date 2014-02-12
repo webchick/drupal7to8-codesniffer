cc13108175104269f93768d580212be3de0a0471
Wim Leers <wim.leers@acquia.com>
2014-02-12 17:14:55 -0500

    Fix the @fixme's leading whitespace.

--- Drupal7to8/Base/FunctionReplacementSniff.php
+++ Drupal7to8/Base/FunctionReplacementSniff.php
@@ -134,9 +134,20 @@ class Drupal7to8_Base_FunctionReplacementSniff extends Generic_Sniffs_PHP_Forbid
     // Prefix the "@fixme" comment with as much whitespace as exists before the
     // function declaration.
     $tokens = $phpcsFile->getTokens();
-    $whitespace = $tokens[$stackPtr - 1]['content'];
+    $this_line = $tokens[$stackPtr]['line'];
+    $first_token_on_this_line = $stackPtr;
+    while ($tokens[$first_token_on_this_line]['line'] == $this_line) {
+      $first_token_on_this_line--;
+    }
+    $first_token_on_this_line++;
 
-    $phpcsFile->fixer->addContentBefore($stackPtr - 1, "\n" . $whitespace . '/** @fixme '. $message . " */\n");
+    $whitespace = '';
+    if ($tokens[$first_token_on_this_line]['type'] === 'T_WHITESPACE') {
+      $first_non_whitespace_token_on_this_line = $phpcsFile->findNext(T_WHITESPACE, $first_token_on_this_line + 1, NULL, TRUE);
+      $whitespace = Drupal7to8_Utility_TokenRange::getContent($tokens, $first_token_on_this_line, $first_non_whitespace_token_on_this_line - 1); //$stackPtr - 1);
+    }
+
+    $phpcsFile->fixer->addContentBefore($first_token_on_this_line, $whitespace . '/** @fixme '. $message . " */\n");
   }
 
   /**

2460f8c3c76292e777b407be9902355551f9e2d8
Gabor Hojtsy <gabor@hojtsy.hu>
2014-02-12 15:31:15 -0500

    Issue #2194833 unify existing language stuff to the language category

--- Drupal7to8/Sniffs/Language/PredefinedListSniff.php
+++ Drupal7to8/Sniffs/Language/PredefinedListSniff.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Drupal7to8_Sniffs_Misc_LocaleGetPredefinedListSniff.
+ * Drupal7to8_Sniffs_Language_PredefinedListSniff.
  *
  * PHP version 5
  *
@@ -16,11 +16,11 @@
  * @package  PHP_CodeSniffer
  * @link     http://pear.php.net/package/PHP_CodeSniffer
  */
-class Drupal7to8_Sniffs_Misc_LocaleGetPredefinedListSniff extends Drupal7to8_Base_FunctionReplacementSniff {
+class Drupal7to8_Sniffs_Language_PredefinedListSniff extends Drupal7to8_Base_FunctionReplacementSniff {
 
   protected $message = '!function() has been converted to a swappable service: https://drupal.org/node/2019329';
 
-  protected $code = 'LocaleGetPredefinedList';
+  protected $code = 'PredefinedList';
 
   protected $forbiddenFunctions = array(
     '_locale_get_predefined_list' => 'LanguageManager::getStandardLanguageList',

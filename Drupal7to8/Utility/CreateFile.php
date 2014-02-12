a9475e576d27913985586dbd9fbcf569b20e54bf
xjm <xjm@65776.no-reply.drupal.org>
2014-02-12 15:22:47 -0500

    Add a utility class for creating files.

---
+++ Drupal7to8/Utility/CreateFile.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Drupal7to8_Utility_CreateFile
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * Utility class for writing out files.
+ *
+ * @todo Add methods for creating yaml files, PSR-N class files, etc.
+ */
+class Drupal7to8_Utility_CreateFile {
+
+  /**
+   * Replace a set of tokens in a boilerplate file template.
+   *
+   * @param string $boilerPath
+   *   The path to the boilerplate file.
+   * @param array $boilerTokens
+   *   An associative array of tokens to replace in the boilerplate file,
+   *   with the tokens as the keys and the values as the replacements
+   *
+   * @return string|null
+   *   The PHP code with the tokens replaced, or NULL on failure.
+   */
+  static public function replaceTokens($boilerPath, array $boilerTokens) {
+    if ($boilerplate = file_get_contents($boilerPath)) {
+      return str_replace(array_keys($boilerTokens), array_values($boilerTokens), $boilerplate);
+    }
+  }
+
+
+}

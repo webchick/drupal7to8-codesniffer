4ed6c53f491163078ae54be334deb786d0933cb6
webchick <webchick@24967.no-reply.drupal.org>
2014-02-12 09:32:20 -0500

    Uh. You can't extend from interfaces it turns out. ;P

--- Drupal7to8/Sniffs/Classes/PsrMoveSniff.php
+++ Drupal7to8/Sniffs/Classes/PsrMoveSniff.php
@@ -16,7 +16,7 @@
  * @package  PHP_CodeSniffer
  * @link     http://pear.php.net/package/PHP_CodeSniffer
  */
-class Drupal7to8_Sniffs_Classes_PsrMoveSniff extends PHP_CodeSniffer_Sniff {
+class Drupal7to8_Sniffs_Classes_PsrMoveSniff implements PHP_CodeSniffer_Sniff {
 
   /**
    * {@inheritdoc}

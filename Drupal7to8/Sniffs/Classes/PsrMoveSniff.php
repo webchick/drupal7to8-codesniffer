eaa638d95a29f4632c9ca53a9b60a21e9ec26c8c
webchick <webchick@24967.no-reply.drupal.org>
2014-02-12 10:53:40 -0500

    Issue #2194001: Add some errors around PSR-0/PSR-4.

--- Drupal7to8/Sniffs/Classes/PsrMoveSniff.php
+++ Drupal7to8/Sniffs/Classes/PsrMoveSniff.php
@@ -19,6 +19,11 @@
 class Drupal7to8_Sniffs_Classes_PsrMoveSniff implements PHP_CodeSniffer_Sniff {
 
   /**
+   * @var Tracks # of classes per file, which can't exceed 1.
+   */
+  protected $classCount = array();
+
+  /**
    * {@inheritdoc}
    */
   public function register()
@@ -32,6 +37,18 @@ class Drupal7to8_Sniffs_Classes_PsrMoveSniff implements PHP_CodeSniffer_Sniff {
    */
   public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
   {
-    // TODO! :)
+    // Increment our class counter for this file.
+    $this->classCount[$phpcsFile->getFilename()]++;
+
+    // Check for a "lib" folder in the filename. If we don't have one, this
+    // is not PSR-* compliant.
+    if (!strstr($phpcsFile->getFilename(), '/lib/') || !strstr($phpcsFile->getFilename(), '/src/' /* Future-proofing */)) {
+      $phpcsFile->addError('Classes should now moved to PSR-* standards: https://drupal.org/node/1320394');
+    }
+
+    // Make sure there is not more than one class in a file.
+    if ($this->classCount[$phpcsFile->getFilename()] > 1) {
+      $phpcsFile->addError('PSR-* dictates only one class definition per file: https://drupal.org/node/1320394');
+    }
   }
 }

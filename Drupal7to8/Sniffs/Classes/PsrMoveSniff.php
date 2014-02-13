11818e44fe1f59883ebd629e550d761a7745d55a
webchick <webchick@24967.no-reply.drupal.org>
2014-02-13 01:15:42 -0500

    Issue #2194001: Better way of checking for more than one class in a file, as well as only printing errors once.

--- Drupal7to8/Sniffs/Classes/PsrMoveSniff.php
+++ Drupal7to8/Sniffs/Classes/PsrMoveSniff.php
@@ -19,9 +19,14 @@
 class Drupal7to8_Sniffs_Classes_PsrMoveSniff implements PHP_CodeSniffer_Sniff {
 
   /**
-   * @var Tracks # of classes per file, which can't exceed 1.
+   * @var int Tracks # of classes per file, which can't exceed 1.
    */
-  protected $classCount = array();
+  protected $classCount = 0;
+
+  /**
+   * @var array Tracks files that were checked, so we only return errors once.
+   */
+  protected $filesChecked = array();
 
   /**
    * {@inheritdoc}
@@ -37,25 +42,41 @@ class Drupal7to8_Sniffs_Classes_PsrMoveSniff implements PHP_CodeSniffer_Sniff {
    */
   public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
   {
-    // Increment our class counter for this file.
-    $this->classCount[$phpcsFile->getFilename()]++;
+    // No need to run these sniffs more than once per file.
+    $filename = $phpcsFile->getFilename();
+    if (isset($this->filesChecked[$filename])) {
+      return;
+    }
+    else {
+      $this->filesChecked[$filename] = 1;
+    }
 
     // Check for a "lib" folder in the filename. If we don't have one, this
-    // is not PSR-* compliant.
-    if (!strstr($phpcsFile->getFilename(), '/lib/') || !strstr($phpcsFile->getFilename(), '/src/' /* Future-proofing */)) {
+    // is not in a PSR-* compliant location.
+
+    if (!strstr($filename, '/lib/') || !strstr($filename, '/src/' /* Future-proofing */)) {
       // Is this a test? Those are special.
-      $fileExtension = strtolower(substr($phpcsFile->getFilename(), -4));
+      $fileExtension = strtolower(substr($filename, -4));
       if ($fileExtension === 'test') {
-        $phpcsFile->addError('Test classes should be moved into a PSR-* compatible Tests directory: https://drupal.org/node/1543796');
+        // @todo Convert this to a fixable error.
+        $phpcsFile->addError('Test classes should be moved into a PSR-* compatible Tests directory: https://drupal.org/node/1543796', $stackPtr);
       }
       else {
-        $phpcsFile->addError('Classes should now moved to PSR-* standards: https://drupal.org/node/1320394');
+        // @todo Convert this to a fixable error.
+        $phpcsFile->addError('Classes should now moved to PSR-* standards: https://drupal.org/node/1320394', $stackPtr);
       }
     }
 
-    // Also make sure there is not more than one class in the file.
-    if ($this->classCount[$phpcsFile->getFilename()] > 1) {
-      $phpcsFile->addError('PSR-* dictates only one class definition per file: https://drupal.org/node/1320394');
+    // Make sure there's not more than one class defined per file.
+    $tokens = $phpcsFile->getTokens();
+    foreach ($tokens as $token) {
+      if ($token['type'] == "T_CLASS") {
+        $this->classCount++;
+      }
+    }
+    if ($this->classCount > 1) {
+      echo $this->classCount;
+      $phpcsFile->addError('PSR-* dictates only one class definition per file: https://drupal.org/node/1320394', $stackPtr);
     }
   }
 }

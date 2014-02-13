17acfc9b0094dd76ccab44bf1bd2a99c47e364c1
xjm <xjm@65776.no-reply.drupal.org>
2014-02-12 22:16:50 -0500

    Refactor ModuleProperties to also get the absolute path to the module.

--- Drupal7to8/Utility/ModuleProperties.php
+++ Drupal7to8/Utility/ModuleProperties.php
@@ -12,14 +12,45 @@
 class Drupal7to8_Utility_ModuleProperties {
 
   /**
-   * Determine the module name based on the file being examined.
+   * Determine the module name for the file being examined.
    *
    * @param PHP_CodeSniffer_File $phpcsFile
    *   The code sniffer file.
+   *
    * @return string|null
    *   The module name if it can be determined, NULL if it cannot.
    */
   static public function getModuleName(PHP_CodeSniffer_File $phpcsFile) {
+    $module_properties = self::getModuleNameAndPath($phpcsFile);
+    return $module_properties['module_name'];
+  }
+
+  /**
+   * Determine the module directory path for the file being examined.
+   *
+   * @param PHP_CodeSniffer_File $phpcsFile
+   *   The code sniffer file.
+   *
+   * @return string|null
+   *   The module name if it can be determined, NULL if it cannot.
+   */
+  static public function getModulePath(PHP_CodeSniffer_File $phpcsFile) {
+    $module_properties = self::getModuleNameAndPath($phpcsFile);
+    return $module_properties['module_path'];
+  }
+
+  /**
+   * Determine the module name and module directory path for the file.
+   *
+   * @param PHP_CodeSniffer_File $phpcsFile
+   *   The code sniffer file.
+   *
+   * @return array
+   *   An array containing:
+   *   - module_name: The name of the module.
+   *   - module_path: The path to the module directory.
+   */
+  static public function getModuleNameAndPath(PHP_CodeSniffer_File $phpcsFile) {
     $file_parts = explode(DIRECTORY_SEPARATOR, $phpcsFile->getFilename());
     // Ignore the filename as we are traversing directories.
     array_pop($file_parts);
@@ -33,9 +64,12 @@ class Drupal7to8_Utility_ModuleProperties {
         array_pop($file_parts);
         continue;
       }
-      return basename($files[0], '.module');
+      $return['module_name'] = basename($files[0], '.module');
+      $return['module_path'] = implode(DIRECTORY_SEPARATOR, $file_parts);
+      break;
     }
 
-    return NULL;
+    return $return;
   }
+
 }
